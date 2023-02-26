cdcbe54a7274ff2a5351137b8b49a7912e62363d
Abdulrahman <abdulrahman.m.hallak.293@gmail.com>
2023-02-27 01:06:36 +0200

    still in the early development stage

---
+++ Gary's GSF/Gary's GSF/Board/Board.cs
@@ -0,0 +1,87 @@
+﻿namespace Gary_s_GSF.Board
+{
+    public class Board
+    {
+        private int _x;
+        private int _y;
+        private List<Tile> _tiles;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int X 
+        { 
+            get { return _x; }
+            set
+            {
+                // Make sure the X is not negative and does not get out of the board.
+                if((X >= 0) && (X <= Width))
+                    _x = X;
+                throw new IndexOutOfRangeException("The X should not exceed the width"); // Edit this exception
+            }
+        }
+        public int Y
+        {
+            get { return _y; }
+            set
+            {
+                // Make sure the Y is not negative and does not get out of the board.
+                if ((Y >= 0) && (Y <= Height))
+                    _y = Y;
+                throw new IndexOutOfRangeException("The X should not exceed the width"); // Edit this exception
+            }
+        }
+
+        public Board(int width , int height)
+        {
+            Width= width;
+            Height= height;
+            InitializeTiles();
+        }
+
+        private void InitializeTiles()
+        {
+            for (int x = 0; x <= Width; x++)
+            {
+                for (int y = 0; y <= Height; y++)
+                {
+                    Tile tile = new Tile(x, y);
+                    _tiles.Add(tile);
+                }
+            }
+        }
+
+        public Tile GetTile(int x, int y)
+        {
+            return;
+        }
+        public void AddUnit()
+        {
+
+        }
+        public void RemoveUnit()
+        {
+
+        }
+
+        public void MoveUnit()
+        {
+
+        }
+        public void ShowUnits()
+        {
+
+        }
+        public void AddTerrain()
+        {
+
+        }
+        public void RemoveTerrain()
+        {
+
+        }
+        public void Battle()
+        {
+
+        }
+
+    }
+}
---
+++ Gary's GSF/Gary's GSF/Board/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gary_s_GSF.Board
+{
+    internal class Tile
+    {
+        public int YCordinate { get; set; }
+        public int XCordinate { get; set;}
+
+        public Tile(int yCordinate, int xCordinate)
+        {
+            YCordinate = yCordinate;
+            XCordinate = xCordinate;
+        }
+
+        public void GetUnits() 
+        { 
+        }
+        public void SetUnits()
+        {
+
+        }
+    }
+}
